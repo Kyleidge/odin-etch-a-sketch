ede358a7def7139aab5b2dbb650760e0e67abbe8
Kyle <7453985+Kyleidge@users.noreply.github.com>
2022-11-13 15:23:12 +0000

    Grid changes color on hover & button created to change grid size

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
-let width = 5;
-let height = 5;
+createGrid(10)
 
-const gridContainerDOM = document.getElementById("grid");
+const newGridButton = document.getElementById("new-grid-button");
 
-for (let row = 0; row < height; row++) {
-    let newRowDOM = document.createElement('div');
-    newRowDOM.className = "row";
+newGridButton.addEventListener("click", () => {
+    createGrid(parseInt(prompt("New grid width/height?")));
+})
 
-    for (let column = 0; column < width; column++) {
-        let newItemDOM = document.createElement('div');
-        newItemDOM.className = "item";
-        newRowDOM.appendChild(newItemDOM);
+function createGrid(elements) {
+    if (elements > 100) { elements = 100; }
+    const gridContainerDOM = document.getElementById("grid");
+    gridContainerDOM.innerHTML = '';
+
+    for (let row = 0; row < elements; row++) {
+        let newRowDOM = document.createElement('div');
+        newRowDOM.className = "row";
+
+        for (let column = 0; column < elements; column++) {
+            let newItemDOM = document.createElement('div');
+            newItemDOM.className = "item";
+            newItemDOM.addEventListener("mouseenter", (e) => {
+                let red = Math.floor(Math.random() * 256);
+                let green = Math.floor(Math.random() * 256);
+                let blue = Math.floor(Math.random() * 256);
+                e.target.style.backgroundColor = "rgb(" + red + "," + green + "," + blue + ")";
+            })
+            newRowDOM.appendChild(newItemDOM);
+        }
+
+        gridContainerDOM.appendChild(newRowDOM);
     }
-
-    gridContainerDOM.appendChild(newRowDOM);
 }
